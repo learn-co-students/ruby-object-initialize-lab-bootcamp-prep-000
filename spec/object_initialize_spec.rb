9bae37c1b21663de05f92200d6010749eaef171f
gj <gj@users.noreply.github.com>
2016-07-13 14:16:53 -0400

    Mut --> Mutt

--- spec/object_initialize_spec.rb
+++ spec/object_initialize_spec.rb
@@ -22,10 +22,10 @@ describe 'Dog' do
       expect(fido.instance_variable_get(:@breed)).to eq("Pug")
     end
 
-    it 'defaults the breed argument to "Mut" in an instance variable @breed' do
+    it 'defaults the breed argument to "Mutt" in an instance variable @breed' do
       fido = Dog.new("Fido")
 
-      expect(fido.instance_variable_get(:@breed)).to eq("Mut")
+      expect(fido.instance_variable_get(:@breed)).to eq("Mutt")
     end
   end
 end
