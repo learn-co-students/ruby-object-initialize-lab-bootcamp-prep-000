726790111ba54058b95f8ab70c2b1bd908b5d91d
SImon Christ <simonchrist@gmx.de>
2017-03-23 15:31:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+
+end
