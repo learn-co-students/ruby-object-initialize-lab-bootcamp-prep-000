99fb6e4fa36f20192150ac49d3b32d0f37ec6ddb
Mehmetcan Budak <mehmetcanbudak@gmail.com>
2017-03-26 19:43:14 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+    def initialize(personName)
+        @name = personName
+    end
+    
+    def name=(personName)
+        @name = personName
+    end
+    
+    def name
+        @name
+    end
+    
+    def job=(personJob)
+        @job = personJob
+    end
+    
+    def job
+        @job
+    end
+    
+end
