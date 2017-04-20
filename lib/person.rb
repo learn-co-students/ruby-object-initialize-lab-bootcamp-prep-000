e9a31a1482d984c157c5fc7a3146f4f950cea4a0
Nancy Noyes <nnoyes@comcast.net>
2017-04-20 18:05:38 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+  def job
+    @job
+  end
+
+end
