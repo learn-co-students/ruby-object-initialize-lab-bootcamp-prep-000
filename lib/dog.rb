63dec068692778977fe47c98d709dfe9c101ef5b
Paul Hosler <pahosler@gmail.com>
2017-06-01 22:43:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+  def initialize(name,breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def breed=(breed)
+    breed = @breed
+  end
+
+  def breed
+    @breed
+  end
+end
