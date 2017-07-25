bf704ba0c0297b287ab119dc97de0b1cea1dec7e
Gene Yoo <gene.y.yoo@gmail.com>
2017-07-25 19:59:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
+end
