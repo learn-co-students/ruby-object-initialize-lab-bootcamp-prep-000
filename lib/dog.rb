c3baf34fa05763de28b55aade068c467a4265b05
Joey Cho <cjy12182000@naver.com>
2017-08-02 23:08:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize(input, inputbreed="Mutt")
+    @name = input
+    @breed =inputbreed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  def initialize(input)
+    @name = input
+  end
+
+  def name
+    @name
+  end
+  
+end
