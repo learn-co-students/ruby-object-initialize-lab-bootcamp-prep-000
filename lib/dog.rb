d8ff62e6db363a85e465e51f10e27904631cf078
Mickey Witchuwong <mickey_aw@hotmail.com>
2017-08-29 19:10:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+   def initialize(name, breed = "Mutt")
+     @name = name
+     @breed = breed
+   end
+
+   def name
+     @name = name
+   end
+
+   def breed
+     @breed = breed
+   end
+end
