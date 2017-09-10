5ec0c34331e519951839a2056310b9fb71841ed0
W M <wmendoz7@calstatela.edu>
2017-09-10 19:23:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+  def initialize(dog_name, a_breed = "Mutt")
+    @name = dog_name
+    @breed = a_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def initialize(person_name)
+    @name = person_name
+  end
+end
