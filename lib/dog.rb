454f1240edc84584bd329b6cf1b297a62a506a2d
Kartik Parihar <kartikvips@gmail.com>
2017-09-14 01:12:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+  def initialize (nombre, breeed = "Mutt")
+    @name = nombre
+    @breed = breeed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def initialize(nombre)
+    @name = nombre
+  end
+end
