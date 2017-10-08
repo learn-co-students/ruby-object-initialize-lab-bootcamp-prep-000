5c7e2327d35541dc064baf6ea82929227e5ab164
Jonathan Rosado <jon.rosado42@gmail.com>
2017-10-08 20:30:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,20 +3,4 @@ class Dog
     @name = name
     @breed = breed
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end
