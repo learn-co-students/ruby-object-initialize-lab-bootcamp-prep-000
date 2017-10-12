2b01d309b9fd87b855f019e3e5c70617e21a3019
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-12 01:37:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  def breed
+    @breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+  def name
+    @name
+  end
+end
