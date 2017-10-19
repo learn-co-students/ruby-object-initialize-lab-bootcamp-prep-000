41d4bfd9da9e3cf8c3ccec92082a25bcf439d67e
Simcha Wood <truthfulthomas@gmail.com>
2017-10-19 23:48:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+  def initialize(my_name, my_breed = "Mutt")
+    @name = my_name
+    @breed = my_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def initialize(my_name)
+    @name = my_name
+  end
+end
