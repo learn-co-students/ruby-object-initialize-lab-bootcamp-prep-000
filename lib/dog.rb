7a615dc4f02ce9559a965e940534d345e1132b06
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-12-06 21:42:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(new_name)
+    @name= new_name
+  end
+
+  def breed
+    @breed
+  end
+end
