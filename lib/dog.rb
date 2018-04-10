d167d638bd49142bbeca4cbaf4513be88d623b23
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-04-10 17:59:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+  class Dog 
+  def initialize(name,breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ def breed=(breed = "Mutt")
+  @breed = breed
+end
+  def breed
+    @breed
+  end
+  def name
+    @name
+  end
+end
