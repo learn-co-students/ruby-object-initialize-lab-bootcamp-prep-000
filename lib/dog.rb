67efd7cac7cff4515f723c9924693557a020c458
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-03 21:58:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+
+class Dog
+  
+  def initialize (name,breed="Mutt")
+     @name=name
+    @breed=breed
+  end
+  def name=(name)
+    @name=name
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+  
+end
