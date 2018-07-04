56ab456046301ba0c886579704dd0037031db2f4
Diana Yoo <diana.yooj@gmail.com>
2018-07-04 01:33:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed=(breed = "Mutt")
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+end
