54e16c27532979b0911936e3711bac1d6cea2fef
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-30 19:00:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  @name 
+  @breed 
+  
+  def initialize (name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  @name 
+  
+  def initialize (name)
+    @name = name
+  end
+  
+end
