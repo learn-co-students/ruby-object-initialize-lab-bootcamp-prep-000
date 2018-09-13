d42fcf681b373b6bbfc783136ac46c233108fb88
yoan ante <yoanante@gmail.com>
2018-09-13 19:59:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,15 @@
 class Dog 
-  def initialize(breed="Mutt")
+  def initialize(name, breed="Mutt")
+    @name = name
     @breed = breed
   end 
+  
+  def name=(name)
+    @name = name
+  end 
+  
+  def name
+    @name
+  end
+
 end 
