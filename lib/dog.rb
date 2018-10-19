ebcda3c4ebd460a0f27bc7d76d70b61afbf06b44
Nathan Bell <nathanbell09@gmail.com>
2018-10-19 14:16:15 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+
+  def initialize(dog_name, dog_breed = "Mutt")
+    @name = dog_name
+    @breed = dog_breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+
+  def initialize(person_name)
+    @name = person_name
+  end
+
+end
