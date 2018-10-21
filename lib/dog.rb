8be41f75ebad7213e78718ce5a37debbd4439e68
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-21 22:49:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed 
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end 
+end 
