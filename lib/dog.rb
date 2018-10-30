a3f8091180d8d5137e38292abb969a84887aa405
gbellini90 <gbellini90@gmail.com>
2018-10-30 15:54:18 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,35 @@
+# LONG WAY OF DOING:
+#class Dog
+#
+#         def initialize(name, breed="Mutt")
+#         @name=name
+#         @breed=breed
+#         end
+
+#         def name=(name)
+#         @name= name
+#         end
+#
+#         def name
+#         @name
+#         end
+#
+#
+#         def breed=(breed)
+#         @breed=breed
+#         end
+#
+#         def breed
+#         @breed
+#         end
+# end
+
+#SHORT WAY OF DOING:
 class Dog
   def initialize(name, breed="Mutt")
     @name=name
     @breed=breed
   end
-  def name=(name)
-    @name= name
-  end
-  def name
-    @name
-  end
-  def breed=(breed)
-    @breed=breed
-  end
-  def breed
-    @breed
-  end
+
+  attr_accessor:name,:breed
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,24 @@
+#LONG WAY OF DOING:
+# class Person
+#     def initialize(name)
+#       @name=name
+#     end
+#
+#     def name=(name)
+#       @name= name
+#     end
+#
+#     def name
+#       @name
+#     end
+#
+# end
+
 class Person
+
   def initialize(name)
     @name=name
   end
-  def name=(name)
-    @name= name
-  end
-  def name
-    @name
-  end
-end
+
+  attr_accessor:name
+end 
