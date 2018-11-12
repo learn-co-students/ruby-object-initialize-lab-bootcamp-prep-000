ec670b7e7e3505928f17184d5b6cf555ec160e7c
Edwin Lin <edwinlin@gmail.com>
2018-11-12 00:15:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
