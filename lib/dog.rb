84b2abd623d2a0f3fc7e1375ac8924fdebae6589
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-07 01:33:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def initialize(name_in, breed_in = "Mutt")
+    @name = name_in
+    @breed = breed_in
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def name
+    @name
+  end
+end
