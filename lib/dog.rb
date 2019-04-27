624cfc7cef6f1553150d1d1110f5e55da2a37cc3
Chris Janson <tightropewalkerr@gmail.com>
2019-04-27 09:40:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def initialize(name, breed="Mutt")
+  @name = name
+  @breed = breed
+end
+
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+  
+  
+end
