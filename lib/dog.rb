9fdc908ea7fbb50283b51936d7a6c0efc9530dc2
Jackson Singleton <jsingleton12321@yahoo.com>
2019-05-01 00:16:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  def name=(dog_name)
+    @name = dog_name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def name
+    @name
+  end
+  def breed
+    @breed
+  end
+end
